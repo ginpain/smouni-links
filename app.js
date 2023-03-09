c822a8028bf253c2011c14b72ddb8960f2136dd7
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-03-09 08:17:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-feather.replace()
+class LinkTo extends HTMLElement {
+    constructor() {
+        super();
+        this.className = 'w3-button w3-round-xlarge w3-theme-l1 w3-border link'
+        this.target = '_blank'
+    }
+}
+
+class HoverShow extends HTMLElement {
+    constructor() {
+        super();
+        this.className = 'w3-button w3-round-xlarge w3-theme-l1 w3-border link'
+        const currentText = this.innerText;
+        this.onmouseover = (ev) => {
+            this.innerText = this.dataset.txt;
+        }
+        this.onmouseleave = (ev) => {
+            this.innerText = currentText;
+        }
+    }
+}
+
+customElements.define("hover-show", HoverShow);
+customElements.define("link-to", LinkTo);
